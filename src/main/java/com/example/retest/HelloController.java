b0664c29408f09c3badfdebf73a743861bc0f84c
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-23 19:21:35 +0900

    cicd test - retest

--- src/main/java/com/example/retest/HelloController.java
+++ src/main/java/com/example/retest/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     @GetMapping("/hello")
     @CrossOrigin()
     public String hello(){
-        return "CICD 작동 테스트 wow!!";
+        return "CICD 작동 테스트 wow!! - this is change";
     }
 }
