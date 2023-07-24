500b97c6f670067aa93ab367fdde5083cdb7b849
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-24 19:38:11 +0900

    cicd deploy.sh - 일단 이렇게하면 되긴 됨

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -50,8 +50,8 @@ sleep 5
 fi
 
 echo "> Deploy - $JAR_PATH "
-#nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
-nohup java -jar $JAR_PATH > /application.log 2> /error.log &
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
 
 
 
