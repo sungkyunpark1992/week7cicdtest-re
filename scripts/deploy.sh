794a7e79eafeb2cff7bef0fad2a372173b8d88b3
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-24 19:23:01 +0900

    cicd ddddd sksksksksks -moduform patch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,10 +31,10 @@
 #nohup java -jar $JAR_NAME > $APP_LOG 2> $ERROR_LOG &
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/moduform
+REPOSITORY=/home/ubuntu/cicdapp
 cd $REPOSITORY
 
-APP_NAME=moduform
+APP_NAME=cicdapp
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
