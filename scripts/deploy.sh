ea0cbafef80f39b7cbef5476159304f0b7fd2658
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-24 18:04:56 +0900

    cicd ddddd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,10 @@ APP_NAME=cicdapp
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+APP_LOG="$APP_NAME/application.log"
+ERROR_LOG="$APP_NAME/error.log"
+DEPLOY_LOG="$APP_NAME/deploy.log"
+
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
@@ -22,4 +26,6 @@ echo "> Deploy - $JAR_PATH "
 #nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 #nohup java -jar $JAR_PATH > /application.log 2> /error.log & 이 안에 범인이 있다. 이게 내 결론이다.
 #nohup java -jar $JAR_PATH > /home/ubuntu/cicdapp/application.log 2> /home/ubuntu/cicdapp/error.log &
-sudo nohup java -jar $JAR_PATH > $REPOSITORY/application.log 2> $REPOSITORY/error.log &
+#sudo nohup java -jar $JAR_PATH > $REPOSITORY/application.log 2> $REPOSITORY/error.log &
+
+nohup java -jar $JAR_NAME > $APP_LOG 2> $ERROR_LOG &
